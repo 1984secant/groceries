8f8d396de48131dd36ceb8185cc8d2eefc3aa640
1984secant <127aacrooms@frhsd.com>
2026-02-04 13:57:50 -0500

    ffffffffffffff

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hey");
+let banana = 0.79;
+let apple = 1.25;
+let bread = 2.99;
+let milk = 3.49;
+let eggs = 4.25;
+
+
+let cartTotal = 0;
+
+
+function addToCart(item){
+    cartTotal += item;
+    return cartTotal;
+}
+
+function removeFromCart(item){
+    cartTotal -= item;
+    return cartTotal;
+}
+
+addToCart(apple);
+addToCart(milk);
+console.log(cartTotal);
+removeFromCart(apple);
+console.log(cartTotal);
+
